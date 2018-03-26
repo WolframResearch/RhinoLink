a139b5c2ddc359c63d0a8e1c76376594073da4f7
tgayley <tgayley@wolfram.com>
2018-03-25 21:10:28 -0500

    Make RhinoUtilities.m more integrated into the paclet.

--- GrasshopperLink/PacletInfo.m
+++ GrasshopperLink/PacletInfo.m
@@ -1,12 +1,10 @@
 Paclet[
     Name -> "GrasshopperLink",
-    Version -> "0.0.2",
+    Version -> "0.9.0",
     MathematicaVersion -> "10+",
     Extensions -> 
         {
             {"Documentation", Language -> "English"}, 
-            {"Kernel", Root -> "Kernel", Context -> "GrasshopperLink`"}
+            {"Kernel", Root -> "Kernel", Context -> {"GrasshopperLink`", "RhinoUtilities`"}}
         }
 ]
-
-
