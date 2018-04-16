5dc7cf02162b9e3a8b9b65761d621cc644a0530c
Chris Carlson <carlson@wolfram.com>
2018-04-16 10:49:35 -0500

    Shortened Grasshopper component labels to single-letters, as is the Grasshopper standard. Made three categories of components: Code, Data, and Extensions.

--- Source/WolframGrasshopperSupport/LinkParam.cs
+++ Source/WolframGrasshopperSupport/LinkParam.cs
@@ -14,7 +14,7 @@ namespace Wolfram.Grasshopper {
 public class LinkParam : GH_Param<LinkType> {
 
     // We need to supply a constructor without arguments that calls the base class constructor.
-    public LinkParam() : base("Link", "seq", "The link to the Wolfram Engine", "Params", "Wolfram", GH_ParamAccess.item) { }
+    public LinkParam() : base("L", "L", "The link to the Wolfram Engine", "Params", "Wolfram", GH_ParamAccess.item) { }
 
     public override System.Guid ComponentGuid {
         get { return new Guid("{b8c0e44f-f815-40f7-9e08-dae4da0813a2}"); }
