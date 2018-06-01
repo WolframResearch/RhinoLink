31adb988a868221e1083eb8912e5d879bcacbfe0
Christopher Carlson <carlson@wolfram.com>
2018-06-01 11:30:08 -0500

    Added documentation to the paclet info.

--- RhinoLink/PacletInfo.m
+++ RhinoLink/PacletInfo.m
@@ -1,10 +1,18 @@
+(* Paclet Info File *)
+
+(* created 2018/05/31*)
+
 Paclet[
     Name -> "RhinoLink",
     Version -> "0.9.0",
-    MathematicaVersion -> "10+",
+    MathematicaVersion -> "11+",
     Extensions -> 
         {
-            {"Documentation", Language -> "English"}, 
-            {"Kernel", Root -> "Kernel", Context -> {"RhinoLink`"}}
+            {"Documentation", Language -> "English", MainPage -> "Guides/RhinoLink"}, 
+            {"Kernel", Root -> "Kernel", Context -> 
+                {"RhinoLink`"}
+            }
         }
 ]
+
+
